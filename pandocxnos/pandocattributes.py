aa7437c2bab275708b98386ce43370ebefb2c1e9
Thomas Duck <tomduck@tomduck.ca>
2019-06-22 14:33:27 -0300

    Adapted for this project.

--- pandocxnos/pandocattributes.py
+++ pandocxnos/pandocattributes.py
@@ -1,5 +1,5 @@
-
 # Copyright 2014-2015 Aaron O'Leary <dev@aaren.me>
+# Copyright 2019 Thomas J. Duck <tomduck@tomduck.ca>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -23,212 +23,160 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+"""pandocattributes.py - pandoc block attributes management.
+
+Usage:
+
+    attrs = '#id .class1 .class2 key=value'
+    attributes = PandocAttributes(attrs, 'markdown')
+
+    attributes.to_pandoc()
+    >>> ['id', ['class1', 'class2'], [['key', 'value']]]
+
+    attributes.to_markdown()
+    >>> '{#id .class1 .class2 key=value}'
+
+    attributes.to_html()
+    >>> id="id" class="class1 class2" key='value'
+
+    attributes.id
+    >>> 'id'
+
+    attributes.classes
+    >>> ['class1', 'class2']
+
+    attributes.kvs
+    >>> OrderedDict([('key', 'value')])
+"""
+
+
 import re
 from collections import OrderedDict
 
-
+# pylint: disable=useless-object-inheritance
 class PandocAttributes(object):
-    """Parser / Emitter for pandoc block attributes.
+    """Parser / Emitter for pandoc block attributes."""
 
-    Can read and write attributes in any of these formats:
-        - markdown
-        - html
-        - dictionary
-        - pandoc
-
-    usage:
-        attrs = '#id .class1 .class2 key=value'
-        attributes = PandocAttributes(attrs, format='markdown')
-
-        attributes.to_markdown()
-        >>> '{#id .class1 .class2 key=value}'
-
-        attributes.to_dict()
-        >>> {'id': 'id', 'classes': ['class1', 'class2'], 'key'='value'}
-
-        attributes.to_html()
-        >>> id="id" class="class1 class2" key='value'
-
-        attributes.to_pandoc()
-        >>> ['id', ['class1', 'class2'], [['key', 'value']]]
-
-        attributes.id
-        >>> 'id'
-
-        attributes.classes
-        >>> ['class1', 'class2']
-
-        attributes.kvs
-        >>> OrderedDict([('key', 'value')])
-    """
     spnl = ' \n'
     split_regex = r'''((?:[^{separator}"']|"[^"]*"|'[^']*')+)'''.format
 
-    parse_failed = False
-    
-    def __init__(self, attrstr=None, format='pandoc'):
+    parse_failed = False  # Flags if not all fields were parsed
+    attrstr = None        # The original attribute string
+
+    def __init__(self, attrstr=None, fmt='pandoc'):
+        """Initializes the attributes."""
         if attrstr is None:
-            id = ''
+            id_ = ''
             classes = []
             kvs = OrderedDict()
-        elif format == 'pandoc':
-            id, classes, kvs = self.parse_pandoc(attrstr)
-        elif format == 'markdown':
-            id, classes, kvs = self.parse_markdown(attrstr)
-        elif format == 'html':
-            id, classes, kvs = self.parse_html(attrstr)
-        elif format == 'dict':
-            id, classes, kvs = self.parse_dict(attrstr)
+        elif fmt == 'pandoc':
+            id_, classes, kvs = self._parse_pandoc(attrstr)
+        elif fmt == 'markdown':
+            id_, classes, kvs = self._parse_markdown(attrstr)
         else:
             raise UserWarning('invalid format')
 
-        self.id = id
+        self.id = id_
         self.classes = classes
         self.kvs = kvs
         self.attrstr = attrstr
 
-    @classmethod
-    def parse_pandoc(self, attrs):
+    @staticmethod
+    def _parse_pandoc(attrs):
         """Read pandoc attributes."""
-        id = attrs[0]
+        id_ = attrs[0]
         classes = attrs[1]
         kvs = OrderedDict(attrs[2])
+        return id_, classes, kvs
 
-        return id, classes, kvs
-
-    @classmethod
-    def parse_markdown(self, attrstr):
+    def _parse_markdown(self, attrstr):
         """Read markdown attributes."""
         attrstr = attrstr.strip('{}')
         splitter = re.compile(self.split_regex(separator=self.spnl))
         attrs = splitter.split(attrstr)[1::2]
 
-        # match single word attributes e.g. ```python
+        # Match single word attributes e.g. python
         if len(attrs) == 1 \
                 and not attrstr.startswith(('#', '.')) \
                 and '=' not in attrstr:
             return '', [attrstr], OrderedDict()
 
         try:
-            id = [a[1:] for a in attrs if a.startswith('#')][0]
+            id_ = [a[1:] for a in attrs if a.startswith('#')][0]
         except IndexError:
-            id = ''
+            id_ = ''
 
         classes = [a[1:] for a in attrs if a.startswith('.')]
         special = ['unnumbered' for a in attrs if a == '-']
         classes.extend(special)
 
-        Nkvs = sum(1 for a in attrs if not a.startswith(('#', '.')))
-        kvs = OrderedDict(a.split('=', 1) for a in attrs if '=' in a and
-                          a[0]!='=' and a[-1]!='=')
+        nkvs = sum(1 for a in attrs if not a.startswith(('#', '.'))
+                   and a != '-')
+        kvs = OrderedDict(a.split('=', 1) for a in attrs
+                          if '=' in a and a[0] != '=' and a[-1] != '=')
 
-        if len(kvs) != Nkvs:
+        if len(kvs) != nkvs:
             self.parse_failed = True
 
-        return id, classes, kvs
+        return id_, classes, kvs
 
-    def parse_html(self, attrstr):
-        """Read a html string to attributes."""
+    def to_pandoc(self):
+        """Returns the attributes as a pandoc-compatibile list."""
+        kvs = [[k, v] for k, v in self.kvs.items()]
+        return [self.id, self.classes, kvs]
 
-        # TO DO: Set parse_failed flag when parse fails.
-        
-        splitter = re.compile(self.split_regex(separator=self.spnl))
-        attrs = splitter.split(attrstr)[1::2]
-
-        idre = re.compile(r'''id=["']?([\w ]*)['"]?''')
-        clsre = re.compile(r'''class=["']?([\w ]*)['"]?''')
-
-        id_matches = [idre.search(a) for a in attrs]
-        cls_matches = [clsre.search(a) for a in attrs]
-
-        try:
-            id = [m.groups()[0] for m in id_matches if m][0]
-        except IndexError:
-            id = ''
-
-        classes = [m.groups()[0] for m in cls_matches if m][0].split()
-
-        special = ['unnumbered' for a in attrs if '-' in a]
-        classes.extend(special)
-
-        kvs = [a.split('=', 1) for a in attrs if '=' in a]
-        kvs = OrderedDict((k, v) for k, v in kvs if k not in ('id', 'class'))
-
-        return id, classes, kvs
-
-    @classmethod
-    def parse_dict(self, attrs):
-        """Read a dict to attributes."""
-        attrs = attrs or {}
-        ident = attrs.get("id", "")
-        classes = attrs.get("classes", [])
-        kvs = OrderedDict((k, v) for k, v in attrs.items()
-                          if k not in ("classes", "id"))
-
-        return ident, classes, kvs
-
-    def to_markdown(self, format='{id} {classes} {kvs}', surround=True):
+    def to_markdown(self, fmt='{id} {classes} {kvs}', surround=True):
         """Returns attributes formatted as markdown with optional
-        format argument to determine order of attribute contents.
+        fmt argument to determine order of attribute contents.
         """
-        id = '#' + self.id if self.id else ''
+        id_ = '#' + self.id if self.id else ''
         classes = ' '.join('.' + cls for cls in self.classes)
         kvs = ' '.join('{}={}'.format(k, v) for k, v in self.kvs.items())
 
-        attrs = format.format(id=id, classes=classes, kvs=kvs).strip()
+        attrs = fmt.format(id=id_, classes=classes, kvs=kvs).strip()
 
         if surround:
             return '{' + attrs + '}'
-        elif not surround:
-            return attrs
+        return attrs
 
     def to_html(self):
         """Returns attributes formatted as html."""
-        id, classes, kvs = self.id, self.classes, self.kvs
-        id_str = 'id="{}"'.format(id) if id else ''
+        id_, classes, kvs = self.id, self.classes, self.kvs
+        id_str = 'id="{}"'.format(id_) if id_ else ''
         class_str = 'class="{}"'.format(' '.join(classes)) if classes else ''
         key_str = ' '.join('{}={}'.format(k, v) for k, v in kvs.items())
         return ' '.join((id_str, class_str, key_str)).strip()
 
-    def to_dict(self):
-        """Returns attributes formatted as a dictionary."""
-        d = {'id': self.id, 'classes': self.classes}
-        d.update(self.kvs)
-        return d
-
-    def to_pandoc(self):
-        kvs = [[k, v] for k, v in self.kvs.items()]
-        return [self.id, self.classes, kvs]
+    @property
+    def list(self):
+        """The attributes in list form."""
+        return self.to_pandoc()
 
     @property
     def markdown(self):
+        """The attributes in html form."""
         return self.to_markdown()
 
     @property
     def html(self):
+        """The attributes in html form."""
         return self.to_html()
 
     @property
-    def dict(self):
-        return self.to_dict()
-
-    @property
-    def list(self):
-        return self.to_pandoc()
-
-    @property
     def is_empty(self):
+        """Returns True if the attributes are empty; False otherwise."""
         return self.id == '' and self.classes == [] and self.kvs == {}
 
     def __getitem__(self, item):
+        """Gets 'id', 'classes', or an attribute."""
         if item == 'id':
             return self.id
-        elif item == 'classes':
+        if item == 'classes':
             return self.classes
-        else:
-            return self.kvs[item]
+        return self.kvs[item]
 
     def __setitem__(self, key, value):
+        """Sets 'id', 'classes', or an attribute."""
         if key == 'id':
             self.id = value
         elif key == 'classes':
@@ -236,5 +184,11 @@ class PandocAttributes(object):
         else:
             self.kvs[key] = value
 
+    def __contains__(self, key):
+        """Returns True if key is 'id', 'classes', or an attribute;
+        False otherwise."""
+        return key == 'id' or key == 'classes' or key in self.kvs
+
     def __repr__(self):
+        """Returns the string representation of self."""
         return "pandocfilters.Attributes({})".format(self.to_pandoc())
